4094eb3dce2e0a8131ca10f2714c743eb17d47d2
Owen Stephens <owen@ostephens.com>
2015-02-03 22:10:52 +0000

    Initial scraper

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,52 @@
-# This is a template for a Ruby scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'nokogiri'
+require 'uri'
+require 'open-uri'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+class District
+    def initialize(uri)
+        @uri = uri
+        @officials = Array.new()
+    end
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+    attr_reader :uri
+    attr_accessor :name, :officials
+
+    def getOfficials
+        begin
+            page = Nokogiri::HTML(open(@uri))
+            page.xpath('//table[@id="ctl00_ContentPlaceHolder1_cab_ubigeo1_gvAutUbigeo"]/tr/td[2]/a').each do |o|
+                  official_name = o.inner_text
+                  official_uri = o.attributes['href']
+                  puts official_name
+                  puts official_uri
+                  @officials.push(Official.new(official_name,official_uri))
+            end
+          rescue
+            puts "Error while getting " + @uri
+          end
+    end
+end
+
+class Official
+    def initialize(name,uri)
+        @uri = uri
+        @name = name
+    end
+
+    attr_reader :name, :uri
+end
+
+scrape_url = "http://infogob.com.pe/Localidad/ubigeo.aspx?IdUbigeo=010102&IdLocalidad=84&IdTab=0"
+d = District.new(scrape_url)
+d.getOfficials
+d.officials.each do |o|
+    puts o.name.to_s
+    puts o.uri.to_s
+    data = {
+        name: o.name.to_s
+        uri: o.uri.to_s
+    }
+    ScraperWiki::save_sqlite([], data)
+end
+
